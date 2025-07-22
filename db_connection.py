11591d87d93425b440bf12d5f1514b48a1d3120c
Taha-Nadeem-7 <160516970+Taha-Nadeem-7@users.noreply.github.com>
2025-07-23 00:03:51 +0500

    db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -6,7 +6,7 @@ import mysql.connector
 def connect_to_db():
     return mysql.connector.connect(
         host="localhost",
-        user="taha",      # or your MySQL user
-        password="taha2005",      # or your MySQL password
+        user="",      # or your MySQL user
+        password="",      # or your MySQL password
         database="restaurant_db"
     )
